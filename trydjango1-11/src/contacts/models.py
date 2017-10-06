2d78c6acffec580b3242dfdceed51ceb60f0cf46
georgesp90 <georgesp90@gmail.com>
2017-10-05 23:58:27 -0400

    signals

--- trydjango1-11/src/contacts/models.py
+++ trydjango1-11/src/contacts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 # Create your models here.
 class UserContacts(models.Model):
@@ -15,6 +18,29 @@ class UserContacts(models.Model):
 	@property 
 	def title(self):
 		return self.name # allows us to say obj.title
+
 	class Meta:
 		verbose_name = 'User Contact'
 		verbose_name_plural ='User Contacts'
+	
+def uc_pre_save_reciever(sender, instance, *args, **kwargs):
+	print('saving..')
+	print(instance.timestamp)
+	# if not instance.slug:
+	# 	instance.slug = unique_slug_generator(instance)
+	# 	instance.save()
+
+
+def uc_post_save_reciever(sender, instance, created, *args, **kwargs):
+	print('saved')
+	print(instance.timestamp)
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+		instance.save()
+
+	
+pre_save.connect(uc_pre_save_reciever, sender=UserContacts)
+
+post_save.connect(uc_post_save_reciever, sender=UserContacts)
+
+	
